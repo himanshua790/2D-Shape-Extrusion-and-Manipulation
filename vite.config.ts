7d3aaa75ad4d513c8711cf69d1958ef619851393
Himanshu Soni <himanshua790@gmail.com>
2024-11-02 15:57:48 +0530

    gitpages

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,25 @@
-import path from 'node:path'
-import { defineConfig, normalizePath } from 'vite'
-import { viteStaticCopy } from 'vite-plugin-static-copy'
+import path from "node:path";
+import { defineConfig, normalizePath } from "vite";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
   plugins: [
     viteStaticCopy({
       targets: [
         {
-          src: normalizePath(path.resolve(__dirname, './node_modules/@babylonjs/havok/lib/esm/HavokPhysics.wasm')),
-          dest: normalizePath(path.resolve(__dirname, './public'))
+          src: normalizePath(
+            path.resolve(
+              __dirname,
+              "./node_modules/@babylonjs/havok/lib/esm/HavokPhysics.wasm"
+            )
+          ),
+          dest: normalizePath(path.resolve(__dirname, "./public")),
         },
-      ]
-    })
+      ],
+    }),
   ],
   worker: {
-    format: 'es',
-  }
-})
+    format: "es",
+  },
+  base: "/2d-shape-extrusion-and-manipulation/",
+});
